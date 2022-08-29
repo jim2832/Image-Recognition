f75d343d0a3f0b829f341c31503b4cad5285b238
Jim Lee <jim880705@gmail.com>
2022-08-29 20:49:56 +0800

    update

--- num.py
+++ num.py
@@ -1,4 +1,14 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("roger.jpg")
-print(img)
+# img = cv2.imread("roger.jpg")
+# print(img.shape)
+
+img = np.empty((300, 300, 3), np.uint8)
+
+for row in range(300):
+    for col in range(300):
+        img[row][col] = [0, 255, 0]
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
