8e0df07694c41c551f6fc0a6b583cbb268d166bb
Jim Lee <jim880705@gmail.com>
2022-08-31 01:44:22 +0800

    update

--- num.py
+++ num.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import random
 
 # img = cv2.imread("roger.jpg")
 # print(img.shape)
@@ -8,7 +9,7 @@ img = np.empty((300, 300, 3), np.uint8)
 
 for row in range(300):
     for col in range(300):
-        img[row][col] = [0, 255, 0]
+        img[row][col] = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
 cv2.imshow("img", img)
 cv2.waitKey(0)
