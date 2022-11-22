a42c5c368ee6ae34de52be5c44c21fbafea81d67
Jim <jim880705@gmail.com>
2022-11-22 17:15:24 +0800

    221122 update

--- image.py
+++ image.py
@@ -2,6 +2,6 @@ import cv2
 
 img = cv2.imread("roger.jpg") #讀取圖片
 
-img = cv2.resize(img, (0, 0), fx = 0.5, fy = 0.5) #調整大小
+img = cv2.resize(img, (0, 0), fx = 0.5, fy = 0.5) #調整大小 #tuple內代表想要調整的大小
 cv2.imshow("img", img) #顯示圖片
 cv2.waitKey(0) #等待時間後關閉(數字代表毫秒) -> 0代表無限
