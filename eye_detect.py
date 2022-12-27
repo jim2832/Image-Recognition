ea197bdece22758a773782f94a6824c500cc5433
Jim <jim880705@gmail.com>
2022-12-28 01:11:40 +0800

    221228 update

--- eye_detect.py
+++ eye_detect.py
@@ -6,14 +6,14 @@ color = (0, 255, 0)
 #轉換成黑白照片 因為人臉辨識不需要用到顏色
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-face_classifier = cv2.CascadeClassifier("face_detect.xml") #人臉識別分類器
-face_rect = face_classifier.detectMultiScale(gray, 1.2, 2) #物件、縮小倍率、目標要被偵測到幾次才算完成
+eye_classifier = cv2.CascadeClassifier("eye_detect.xml")
+eye_rect = eye_classifier.detectMultiScale(gray, 1.5, 5)
 
-print("face detected: ", len(face_rect)) #印偵測到幾張臉
-print(face_rect)
+print("eye detected: ", len(eye_rect)) #印偵測到幾張臉
+print(eye_rect)
 
 #偵測
-for (x, y, w, h) in face_rect:
+for (x, y, w, h) in eye_rect:
     cv2.rectangle(img, (x, y), (x+w, y+h), color, 1) #畫出正方形
 
 cv2.imshow("img", img)
